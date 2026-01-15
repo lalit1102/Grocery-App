47259d7be90b7bbb4be53c8c1a3df5fea9ae2ae1
Lalit Baldaniya <lalit.baldaniya92@gmail.com>
2026-01-15 18:54:32 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer/Footer'
 
 
 
@@ -19,6 +20,7 @@ const App = () => {
       <Route path='/' element={<Home />} />
     </Routes>
    </div>
+  {!isSellerPath && <Footer />}
    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,16 @@ export default {
   theme: {
     extend: {
       screens: {
-        // Default Tailwind breakpoints:
+       
         sm: '640px',
-        md: '768px',   // iPad Mini portrait
-        lg: '1024px',  // iPad Pro landscape
+        md: '768px',   
+        lg: '1024px',  
         xl: '1280px',
         '2xl': '1536px',
 
-        // Custom breakpoints for more control:
-        ipadMini: '820px',   // covers iPad Mini landscape (~820px)
-        ipadPro: '1024px',   // covers iPad Pro portrait/landscape
+       
+        ipadMini: '820px',   
+        ipadPro: '1024px',   
       },
     },
   },
